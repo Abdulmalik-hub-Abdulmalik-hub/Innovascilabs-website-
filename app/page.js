12d822117602a43c2ef3399c6cf1a386ea4820d4
Abdulmalik-hub-Abdulmalik-hub <abdulmalikmusba@gmail.com>
2026-03-29 22:45:21 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+hereexport default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>🚀 InnovaSci AI Labs</h1>
+      <p>Site is working successfully 🎉</p>
+    </main>
+  );
+  }
