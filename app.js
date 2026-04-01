93f17282954231602311e313768d9d4ae03c2f9f
Abdulmalik-hub-Abdulmalik-hub <abdulmalikmusba@gmail.com>
2026-04-01 14:04:50 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-hereexport default function Home() {
-  return (
-    <main style={{ padding: "40px", textAlign: "center" }}>
-      <h1>🚀 InnovaSci AI Labs</h1>
-      <p>Site is working successfully 🎉</p>
-    </main>
-  );
-  }
