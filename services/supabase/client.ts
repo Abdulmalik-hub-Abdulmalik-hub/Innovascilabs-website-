22f6b4363f0e7df4644833c89a797eab9c77e566
Abdulmalik-hub-Abdulmalik-hub <abdulmalikmusba@gmail.com>
2026-04-04 06:48:41 +0100

    Update client.ts

--- services/supabase/client.ts
+++ services/supabase/client.ts
@@ -1,4 +1,13 @@
 // services/supabase/client.ts
+// @ts-ignore
+import { createBrowserClient } from '@supabase/ssr';
+
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
 import { createClient } from '@supabase/supabase-js';
 
 /**
