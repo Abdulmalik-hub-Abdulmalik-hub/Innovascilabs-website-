7609d961bc5adc67e5fcad8132f65ab4267a873c
Abdulmalik-hub-Abdulmalik-hub <abdulmalikmusba@gmail.com>
2026-04-04 07:16:49 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-here/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // This allows the build to succeed even if there are Type errors
+    // This allows the build to succeed even if there are TypeScript errors
     ignoreBuildErrors: true,
   },
   eslint: {
