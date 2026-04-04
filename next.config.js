5b94dc04a3dd8f16ec25fd93a0c706dab2b5b028
Abdulmalik-hub-Abdulmalik-hub <abdulmalikmusba@gmail.com>
2026-04-04 08:05:48 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // This allows the build to successfully complete even if there are Type errors
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // We removed 'eslint' here because Next.js 16 no longer supports it in this format
 }
 
 module.exports = nextConfig
